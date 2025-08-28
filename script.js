1603b5480147185307b4caef585ffe6301262081
LidiaAlemu <lidiadanialemu@gmail.com>
2025-08-28 14:27:45 -0400

    feat(js): add cart button functionality, update cart total, and toggle highlight class

--- script.js
+++ script.js
@@ -17,7 +17,7 @@ function formatProduct(name, price) {
     return `${name} - $${price}`;
 }
 
-// Part 3: Loops
+
 let products = [
     {name: "T-Shirt", price: 25},
     {name: "Jeans", price: 50},
@@ -29,4 +29,20 @@ products.forEach(product => {
     let li = document.createElement("li");
     li.textContent = formatProduct(product.name, product.price);
     productsList.appendChild(li);
+});
+
+
+let cart = [];
+let checkoutBtn = document.getElementById("checkout-btn");
+checkoutBtn.addEventListener("click", () => {
+    
+    let randomProduct = products[Math.floor(Math.random() * products.length)];
+    cart.push({ ...randomProduct, quantity: 1 });
+
+    
+    let cartTotal = document.getElementById("cart-total");
+    cartTotal.textContent = "Total: $" + calculateTotal(cart);
+
+    
+    document.getElementById("store-header").classList.toggle("highlight");
 });
